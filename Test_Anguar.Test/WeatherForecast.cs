b3f23c5b28ce9dea266fb4f31805a69f4b8396af
thisaraniroshan <thisaraniroshan1994@gmail.com>
2022-10-15 13:30:30 +0530

    testB

--- Test_Anguar.Test/WeatherForecast.cs
+++ Test_Anguar.Test/WeatherForecast.cs
@@ -9,5 +9,7 @@ namespace Test_Anguar.Test
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+        public string newC { get; set; }
+        public string newB { get; set; }
     }
 }
